7ae3d855d6164bb98e7ee7c4ae337eb21b87269a
m-n-zuk <magdalena.n.zuk@gmail.com>
2023-03-28 23:56:29 +0200

    add task_6 (main.tf & README.md)

---
+++ task_6/README.md
@@ -0,0 +1,9 @@
+# task_6
+
+### task:
+> 1. Create main.tf file, configure the provider and add configuration
+> 2. Create Cloud Storage bucket that has public access configured
+> 3. Upload manually the index.html file (that you previously created in Task 1) into the newly created bucket
+
+### URL of the object (index.html) from your bucket:
+> https://storage.googleapis.com/task_6_cloud/index.html
---
+++ task_6/main.tf
@@ -0,0 +1,67 @@
+provider "google" {
+  project = "data-tangent-378423"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_storage_default_object_access_control" "public_rule" {
+  bucket = google_storage_bucket.bucket.name
+  role   = "READER"
+  entity = "allUsers"
+}
+
+resource "google_storage_bucket" "bucket" {
+  name = "task_6_cloud"
+  project = "data-tangent-378423"
+  storage_class = "standard"
+  location = "EU"
+}
+
+resource "google_compute_instance" "dare-id-vm" {
+  name         = "my-dareit-vm-tf"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  tags = ["dareit"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        managed_by_terraform = "true"
+      }
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+}
+
+resource "google_sql_database_instance" "instance" {
+  name             = "postgres-task-6"
+  database_version = "POSTGRES_14"
+  root_password    = "abcABC123!"
+  settings {
+    tier = "db-custom-2-7680"
+  }
+  deletion_protection = false
+}
+
+resource "google_sql_database" "database" {
+  name = "dareit"
+  instance = "postgres-task-6"
+  charset = "utf8"
+  collation = "utf8_general_ci"
+}
+
+resource "google_sql_user" "users" {
+  name = "dareit_user"
+  instance = "postgres-task-6"
+  host = "%"
+  password = "DareITpassw0rd"
+}
